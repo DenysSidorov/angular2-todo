702071576cc39d8dcb947328e5ce2f519c1a3275
DenysSidorov <000scorpions000@gmail.com>
2016-11-06 14:05:27 +0200

    add ngFor

--- app/main.ts
+++ app/main.ts
@@ -1,16 +1,30 @@
 /// <reference path="../node_modules/angular2/ts/typings/node/node.d.ts"/>
 /// <reference path="../node_modules/angular2/typings/browser.d.ts"/>
 
-import { Component } from "angular2/core"
-import { bootstrap } from "angular2/platform/browser"
+import {Component} from "angular2/core";
+import {bootstrap} from "angular2/platform/browser";
 
 @Component({
     selector: 'my-dir',
-    template: '<h1>1123123 World</h1>'
+    template: `
+                <div>
+                    <h1>{{ title }}</h1>
+                    <ul>
+                    <li *ngFor="#item of arrray">{{ item }}</li>
+                    </ul>
+                </div>
+`
 })
 
-class AppComponent{
+class AppComponent {
 
+    title: string;
+    arrray: string[];
+
+    constructor() {
+        this.title = 'Заголовок';
+        this.arrray = ['Выучить Angular 2', 'Выучить TypeScript 2'];
+    }
 }
 
 bootstrap(AppComponent);
